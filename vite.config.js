b1ff7d865796d6c0831b667ff2231d35f5c6491b
joseph961203 <joseph961203@gmail.com>
2025-05-23 01:27:01 -0700

    Update the vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["d603a7ad9b4e3-8088.us-east-a.proxy.x-gpu.co"],
+  },
+});
